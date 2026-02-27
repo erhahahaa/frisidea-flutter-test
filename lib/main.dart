fd7c5a1312b451f78da311f32bef7f5edea524e6
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2026-02-27 09:06:55 +0700

    fix: initialize date formatting in main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'screens/home_screen.dart';
 
-void main() {
+void main() async {
+  await initializeDateFormatting('id_ID', null);
   runApp(const MyApp());
 }
 
